7b603a9aefc5a6675fdf19912463fed5b9235d32
Carlos Cenci <61289109+carloscenci@users.noreply.github.com>
2021-06-05 09:56:37 -0300

    Apply finish

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <h1>Cataline</h1>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
